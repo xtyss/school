ae952d34261529656259556045c6bbfc67e1a91a
yssgg <199693ys>
2020-07-13 16:51:32 +0800

    Initial commit

---
+++ src/test/java/com/ys/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
